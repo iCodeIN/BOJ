4ddeeadf7b54a670624d3302f365f0486d224700
Chris Ohk <utilforever@gmail.com>
2017-01-28 21:11:22 +0900

    Upload 2557 - Hello World

--- C++/2557 - Hello World.cpp
+++ C++/2557 - Hello World.cpp
@@ -1,3 +1,10 @@
+/*************************************************************************
+> File Name: 2557 - Hello World.cpp
+> Author: Chan-Ho Chris Ohk
+> Created Time: 2017/01/28
+> Problem Link: https://www.acmicpc.net/problem/2557
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
 #include <iostream>
 
 int main(int argc, char* argv[])
