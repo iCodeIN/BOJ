8a4c2812d528e3409271d3a772597f84d7363ecb
Chris Ohk <utilforever@gmail.com>
2017-01-28 21:19:46 +0900

    Upload 1000 - A + B

---
+++ C++/1000 - A + B.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+> File Name: 1000 - A + B.cpp
+> Author: Chan-Ho Chris Ohk
+> Created Time: 2017/01/28
+> Problem Link: https://www.acmicpc.net/problem/1000
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	int a = 0, b = 0;
+	std::cin >> a >> b;
+
+	std::cout << a + b << std::endl;
+}
