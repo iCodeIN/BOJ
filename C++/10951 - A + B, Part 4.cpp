55cfeb3a25f16dd79b8e67799a7bc1eba878c76a
Chris Ohk <utilforever@gmail.com>
2017-01-29 13:23:37 +0900

    Upload 10950 - A + B, Part 4

---
+++ C++/10951 - A + B, Part 4.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+> File Name: 10951 - A + B, Part 4.cpp
+> Author: Chan-Ho Chris Ohk
+> Created Time: 2017/01/29
+> Problem Link: https://www.acmicpc.net/problem/1000
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	int a = 0, b = 0;
+
+	while (std::cin >> a >> b)
+	{
+		std::cout << a + b << std::endl;
+	}
+
+	return 0;
+}
