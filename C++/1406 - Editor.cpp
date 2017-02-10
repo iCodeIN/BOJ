75cf581a576d2b3027d672fff620da8d83a09a5b
Chris Ohk <utilforever@gmail.com>
2017-02-10 23:54:54 +0900

    Upload 1406 - Editor

---
+++ C++/1406 - Editor.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+> File Name: 1406 - Editor.cpp
+> Author: Chan-Ho Chris Ohk
+> Created Time: 2017/02/10
+> Problem Link: https://www.acmicpc.net/problem/1406
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <iostream>
+#include <stack>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+	std::string input;
+
+
+	return 0;
+}
